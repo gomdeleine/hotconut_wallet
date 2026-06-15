f0df49b23d7869fff99df98f9353d8d021820351
ella <eunji@noncelab.com>
2026-06-15 15:30:07 +0900

    fix(intExtension)

--- lib/extensions/int_extensions.dart
+++ lib/extensions/int_extensions.dart
@@ -3,6 +3,6 @@ import 'package:coconut_wallet/utils/numeric_input_formatters.dart';
 
 extension IntFormatting on int {
   String toThousandsSeparatedString() {
-    return formatIntWithGroupingSeparator(this, NumberFormatConfig.instance.groupingSeparator);
+    return formatIntWithGroupingSeparator(toString(), NumberFormatConfig.instance.groupingSeparator);
   }
 }
